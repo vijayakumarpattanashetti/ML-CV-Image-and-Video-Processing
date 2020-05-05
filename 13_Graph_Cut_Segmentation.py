1d90070df177e0f15626f153745cb582f5583cbd
vijayakumarpattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2020-05-05 22:05:33 +0530

    Update 13_Graph_Cut_Segmentation.py

--- 13_Graph_Cut_Segmentation.py
+++ 13_Graph_Cut_Segmentation.py
@@ -1,15 +1,3 @@
-"""
-Key '0' - To select areas of sure background
-Key '1' - To select areas of sure foreground
-Key '2' - To select areas of probable background
-Key '3' - To select areas of probable foreground
-Key 'n' - To update the segmentation
-Key 'r' - To reset the setup
-Key 's' - To save the results
-===============================================================================
-"""
-
-# Python 2/3 compatibility
 from __future__ import print_function
 
 import numpy as np
